b7632a25ecc2a2503ee6f9a4fa9eccc8eaac2095
Eduardo <eduardodmz@gmail.com>
2013-12-21 17:05:28 -0600

    - guardando datos en disco

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ cofcof
 
 Parsea, almacena y hace públicos los datos de calidad del aire que son
 publicados por el gobierno del Estado de Nuevo Léon.
+
+
+http://www.nl.gob.mx/?P=sima_metropolitano
+http://www.cemda.org.mx/08/proponen-academicos-organzaciones-sociales-y-la-cdhdf-modificar-la-norma-del-imeca/
+
--- parse_sima.rb
+++ parse_sima.rb
@@ -1,7 +1,41 @@
 require "mechanize"
+require "sequel"
+require "date"
+
+#require "./config/environments"
+
+
+# connect to an in-memory database
+DB = Sequel.sqlite('./db/metrics.sqlite3')
+
+# create an items table
+if !DB.table_exists? :metrics
+  DB.create_table :metrics do
+    FixNum :station_id
+    Date :day
+    Fixnum :hour
+    Float :pm10
+    Float :ozone
+    Float :co
+    Float :so2
+    Float :no2
+    Float :pm25
+    Float :wind_speed
+    Float :temperature
+    Float :relative_humidity
+    Float :wind_direction
+    Float :average_pressure
+    Float :atmospheric_pressure
+    Float :milimeters_of_rain
+    Float :solar_radiation
+    Fixnum :counter
+    primary_key [:station_id, :day, :hour], name: "metrics_pk", auto_increment: false
+  end  
+end
+
 class SimaParser
   
-  AVAILABLE_STATION_IDS = [1, 2, 3, 4, 5, 6, 7, 8,   
+  AVAILABLE_STATION_IDS = [1, 2, 3, 4, 5, 6, 7, 8, 11]
   SIMA_WEBROOT          = "http://www.nl.gob.mx/?P=sima_metropolitano&url=SistemaGubernamentales/sima/ReporteDiarioDetallado.aspx"
   
   def initialize
@@ -9,14 +43,29 @@ class SimaParser
   end
   
   def parse_station(station_number)
-    page             = @agent.get(station_url(station_number))        
-    return { 
-      timestamp:  Time.now.to_i,
-      station_id: station_number,
-      station_name: page.search("#divUbicacion").children.first.to_s.strip,
-      contaminants: get_data_in_hash(page, "#dgContaminantes  tr.itemStyle"),
-      weather_data: get_data_in_hash(page, "#dgMeterologicos  tr.itemStyle")
-    }
+    if page = get_page(station_number)
+      return { 
+        timestamp:  Time.now.to_i,
+        station_id: station_number,
+        station_name: page.search("#divUbicacion").children.first.to_s.strip,
+        contaminants: get_data_in_hash(page, "#dgContaminantes  tr.itemStyle"),
+        weather_data: get_data_in_hash(page, "#dgMeterologicos  tr.itemStyle")
+      }      
+    else
+      return { }
+    end
+  end
+  
+  def get_page(station_number)
+    page         = nil
+    num_attempts = 0
+    begin
+      num_attempts += 1
+      page          = @agent.get(station_url(station_number))            
+    rescue StandardError
+      retry if num_attempts <= 3
+    end
+    return page
   end
   
   def convert_label_to_symbol(label_str)
@@ -45,7 +94,7 @@ class SimaParser
   
   def get_data_in_hash(page, xpath_search)
     array_of_values = collect_table_values(page, xpath_search)
-    array_of_values.inject(Hash.new) do |memo, object|
+    array_of_values.inject({ }) do |memo, object|
       metric_name               = object[0]
       memo[metric_name]         = { value: convert_string_to_usable_type(object[1]) }
       memo[metric_name][:extra] = object[2] if object.count == 3
@@ -80,8 +129,31 @@ class SimaParser
   
 end
 
+def build_key_value_pairs_for_info(station_info)
+  kvps = station_info[:contaminants].inject({ }) do |memo,object|
+    memo[object[0]] = object[1][:value]
+    memo
+  end
+  kvps = station_info[:weather_data].inject(kvps) do |memo,object|
+    memo[object[0]] = object[1][:value]
+    memo
+  end
+  kvps[:day]  = Time.now.day
+  kvps[:hour] = Time.now.hour  
+  kvps
+end
 
-1.upto(24) do
-  p SimaParser.new().parse_available_stations()
+metrics = items = DB[:metrics]
+1.upto(100) do 
+  SimaParser.new().parse_available_stations().each_pair do |station_id, station_info|  
+    kvps = build_key_value_pairs_for_info(station_info)
+    metrics.insert({ station_id: station_id}.merge(kvps))
+  end
+  
+  puts Time.now
   sleep(60*60)
 end
+
+
+
+
